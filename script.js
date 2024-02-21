3004850d270996a6a97a288f2999983ef1243f78
Tuddeepana <tauddeepana@gmail.com>
2024-02-21 15:33:51 +0530

    file add

---
+++ script.js
@@ -0,0 +1,14 @@
+//days hours minitues
+// 01 Jan 00:00
+
+let cDay;
+let cHour;
+let cMin;
+let cSec;
+
+let interval = window.setInterval(function (params) {
+
+    let cDate = new Date();
+    console,console.log(cDate);
+
+},1000);
