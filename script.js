f26a44050bf127589091d92fedc511af4684ca78
Tuddeepana <tauddeepana@gmail.com>
2024-02-21 16:03:36 +0530

    set the tims js

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ let cSec;
 let interval = window.setInterval(function (params) {
 
     let cDate = new Date();
-    console,console.log(cDate);
+    cHour = cDate.getHours();
+    cmin = cDate.getMinutes();
+    cSec =cDate.getSeconds();
+    cDay = cDate.getDate();
+    //console,console.log();
+
+
+    document.getElementById('sec').innerHTML=60-cSec;
+    document.getElementById('min').innerHTML=60-cmin;
+    document.getElementById('hours').innerHTML=24-cHour;
+    document.getElementById('days').innerHTML=365-cDay;
 
 },1000);
+
